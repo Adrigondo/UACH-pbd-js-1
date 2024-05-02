6ba462d7cb86e6045018e31c71d845cb7d287acb
Adrian Gonzalez <adrigondo.profesional@gmail.com>
2024-05-02 00:30:11 -0600

    Exercise 21

---
+++ 21.js
@@ -0,0 +1,16 @@
+const forLoop = (lowerLimit, upperLimit, step) => {
+    return new Promise((resolve, reject) => {
+        let sum = 0;
+        for (let i = lowerLimit; i <= upperLimit; i += step) {
+            sum += i;
+        }
+        resolve(sum);
+    })
+}
+
+forLoop(1, 1000, 2).then((oddSum) => {
+    console.log(`The sum of odd numbers is ${oddSum}`);
+});
+forLoop(0, 1000, 2).then((evenSum) => {
+    console.log(`The sum of even numbers is ${evenSum}`);
+});
