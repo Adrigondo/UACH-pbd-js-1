7a20fb7a86b9992f98bb1998ddc51e545daaf8e6
Adrian Gonzalez <adrigondo.profesional@gmail.com>
2024-05-02 00:31:35 -0600

    Exercise 32

---
+++ 32.js
@@ -0,0 +1,26 @@
+const people = [
+    "Juan",
+    "María",
+    "José",
+    "Guadalupe",
+    "Francisco",
+    "Ana",
+    "Jesús",
+    "Margarita",
+    "Antonio",
+    "Luz",
+    "Carlos",
+    "Sofía",
+    "Manuel",
+    "Laura",
+    "Pedro",
+    "Isabel",
+    "Miguel",
+    "Patricia",
+    "Alejandro",
+    "Verónica"
+];
+
+people.forEach(name => {
+    console.log(name);
+});
